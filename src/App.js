89c6058909a0e72480bb3688aade7689d69223d8
won-n-only <siliver12@gmail.com>
2024-09-14 02:01:56 +0900

    feat: route ReservationCreatePage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 import OnConstruction from './pages/OnConstruction'
+import ReservationCreatePage from './pages/Reservation/Create';
 // import ReservationDetail from './pages/Reservation/Detail';
-// import ReservationCreate from './pages/Reservation/Create';
 // import ReservationList from './pages/Reservation/List';
 // import AccountRegister from './pages/Account/Register';
 // import TermsPolicy from './pages/Policy';
@@ -15,11 +15,11 @@ function App() {
       <Routes>
         <Route path="/MainPage" element={<MainPage />} />
         <Route path="/" element={<LoginPage />} />
-        <Route path="/OnConstruction" element={<OnConstruction />} />
         <Route path="/MyPage" element={<MyPage />} />
-        {/* <Route path="/reservation/detail" element={<ReservationDetail />} /> */}
-        {/* <Route path="/reservation/create" element={<ReservationCreate />} /> */}
-        {/* <Route path="/reservation/list" element={<ReservationList />} /> */}
+        <Route path="/OnConstruction" element={<OnConstruction />} />
+        <Route path="/ReservationCreatePage" element={<ReservationCreatePage />} />
+        {/* <Route path="/Reservation/detail" element={<ReservationDetail />} /> */}
+        {/* <Route path="/Reservation/list" element={<ReservationList />} /> */}
         {/* <Route path="/account/register" element={<AccountRegister />} /> */}
         {/* <Route path="/terms-policy" element={<TermsPolicy />} /> */}
       </Routes>
