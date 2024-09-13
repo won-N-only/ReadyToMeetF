8ee986b9ccdac82226e38c243b35bcd1608da8a9
won-n-only <siliver12@gmail.com>
2024-09-14 00:46:58 +0900

    design: redesign reservation buttons

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -13,34 +13,32 @@ function MainPage() {
     <div className="flex flex-col items-center min-h-screen bg-white px-4">
       {/* 상단 로고와 사용자 아이콘 */}
       <div className="flex justify-between items-center w-full py-4">
-        <div className="text-6xl font-bold text-[#0046ff]">M</div>
-        <h1 className="text-3xl font-bold text-[#0046ff] mt-1">메인 페이지</h1>
+        <button onClick={navigateToMainPage} className="text-6xl text-[#0046af] font-extrabold">M</button>
+        <h1 className="text-3xl text-[#0046af] mt-1 font-extrabold">메인 페이지</h1>
         <div className="text-3xl text-gray-700">
-          <FaUserCircle className="text-5xl text-[#0046ff]"></FaUserCircle>
+          <FaUserCircle className="text-5xl text-[#0046af]"></FaUserCircle>
         </div>
       </div>
 
-      {/* 예약 생성 및 예약 참여 버튼 섹션 */}
-      <div className="flex justify-between items-center w-full mt-8">
-        <div className="flex flex-col items-center w-full">
-          {/* 예약 생성 이미지 */}
-          <img src="/buttonIcons/calendarPlus.png" alt="예약 생성" className="w-full h-auto" />
-          <h1 className="text-[#0046ff] text-xl py-2 px-6 mt-4">예약 생성</h1>
+      {/* 예약 생성 및 예약 참여 버튼 틀 */}
+      <div className="container mx-auto flex justify-between items-center mt-16 max-w-2xl">
+        <div className="flex flex-col items-center flex-grow">
+          <img src="/buttonIcons/calendarPlus.png" alt="예약 생성" className="h-auto" />
+          <h1 className="text-[#0046af] text-4xl mt-2 pb-6 font-bold">예약 생성</h1>
         </div>
 
-        <div className="flex flex-col items-center w-full">
-          {/* 예약 참여 이미지 */}
-          <img src="/buttonIcons/calendarCheck.png" alt="예약 참여" className="w-full h-auto" />
-          <h1 className="text-[#0046ff] text-xl py-2 px-6 mt-4">예약 참여</h1>
+        <div className="flex flex-col items-center flex-grow">
+          <img src="/buttonIcons/calendarCheck.png" alt="예약 참여" className="h-auto" />
+          <h1 className="text-[#0046af] text-4xl mt-2 pb-6 font-bold">예약 참여</h1>
         </div>
       </div>
-
 
       {/* 진행중인 예약 리스트 틀 */}
-      <div className="w-full mt-12">
-        <h2 className="text-xl font-bold text-[#0046ff]">진행중인 예약</h2>
+      <div className="container mx-auto mt-12 max-w-2xl">
+        <h2 className="text-xl font-bold text-[#0046af]">진행중인 예약</h2>
+        <hr className='border-solid 3px mt-3'></hr>
         <div className="grid grid-cols-3 gap-4 mt-4 text-gray-600">
-          <div className="font-bold">예약</div>
+          <div className="font-bold">  예    약</div>
           <div className="font-bold">예약 상태</div>
           <div className="font-bold">예약자 유형</div>
         </div>
