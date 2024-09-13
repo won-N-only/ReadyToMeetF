3105c723052e261a6cb58e79f86b73c67b4e1a9c
won-n-only <siliver12@gmail.com>
2024-09-14 00:02:23 +0900

    feat: router setting

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
+// import ReservationDetail from './pages/Reservation/Detail';
+// import ReservationCreate from './pages/Reservation/Create';
+// import ReservationList from './pages/Reservation/List';
+// import AccountRegister from './pages/Account/Register';
+// import MyPage from './pages/MyPage';
+// import TermsPolicy from './pages/Policy';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/reservation/detail" element={<ReservationDetail />} /> */}
+        {/* <Route path="/reservation/create" element={<ReservationCreate />} /> */}
+        {/* <Route path="/reservation/list" element={<ReservationList />} /> */}
+        {/* <Route path="/account/register" element={<AccountRegister />} /> */}
+        {/* <Route path="/MyPage" element={<MyPage />} /> */}
+        {/* <Route path="/terms-policy" element={<TermsPolicy />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
